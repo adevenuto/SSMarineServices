4ac57c855843e670dffc643be23a394f7deea3da
Anthony <anthonydevenuto@gmail.com>
2016-12-15 17:22:00 -0600

    formated search results page

--- wp-content/themes/ss-marine-services/search.php
+++ wp-content/themes/ss-marine-services/search.php
@@ -8,41 +8,32 @@
  */
 
 get_header(); ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'ss-marine-services' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
+	<div class="container pages">
+		<div class="row">
+			<div class="col-sm-9 search-results">
+				<h1 class="page-title"><?php printf( esc_html__( 'Results for: %s', 'ss-marine-services' ), '<span>' . get_search_query() . '</span>' ); ?>
+				</h1>
+				<?php
+				if ( have_posts() ) : ?>
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'search' );
+					endwhile;
+					the_posts_navigation();
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif; ?>
+			</div>
+			<div class="col-sm-3 sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
 <?php
-get_sidebar();
 get_footer();
--- wp-content/themes/ss-marine-services/template-parts/content-search.php
+++ wp-content/themes/ss-marine-services/template-parts/content-search.php
@@ -11,20 +11,35 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php ss_marine_services_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else : ?>
+			<?php if (has_post_thumbnail()) { ?>
+				<div class="post-image">
+					<a href="<?php the_permalink() ?>">
+						<?php the_post_thumbnail() ?>
+					</a>
+					<div class="post-tags">
+						<?php the_tags('',',',''); ?>
+					</div>
+				</div>
+			<?php } ?>
+			<h4><a href="<?php the_permalink() ?>"><?php the_title_limit(50, '...'); ?></a></h4>
 		<?php endif; ?>
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="post-details">
+				<div class="post-comments-badge">
+					<a href=""><?php comments_number(0, 1, '%'); ?></a>
+				</div>
+  			<strong><?php the_author();?></strong>
+				</i><time><?php the_date(); ?></time>
+				<?php edit_post_link('Edit', '<div><i class="fa fa-pencil"></i> ', '</div>'); ?>
+			</div><!-- post-details -->
+		<?php
+		endif; ?>
 	</header><!-- .entry-header -->
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php ss_marine_services_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<div class="post-excerpt">
+		<?php echo excerpt(30) ?><a href="<?php the_permalink() ?>">Continue Reading</a>
+	</div>
 </article><!-- #post-## -->
